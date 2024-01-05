7e809d41a6bbac9ba77473e46132e8dbc582b157
ShypkoStanislav <119887312+ShypkoStanislav@users.noreply.github.com>
2024-01-06 01:58:08 +0200

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 function GET(req, res, url, payload) {
     const ip = res.socket.remoteAddress;
     const port = res.socket.remotePort;
-    res.json({name: `My main index page`})
+    res.json({name: `My main page`})
 }
 
 function OPTIONS(req, res, url, payload) {
